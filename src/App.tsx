640487f10eda1239b557427cff1ce184846e0474
tanverrana <tanversobur@gmail.com>
2022-05-30 11:46:35 +0600

    data type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+let student: string = "tammim";
+let age: number = 5;
+let isSmart: boolean = true;
+
+let students: String[] = ["tnaver", "Rana"];
+
 function App() {
   return (
     <div className="App">
---
+++ src/vanila/vanila.js
@@ -0,0 +1,7 @@
+let user = "Tanver";
+user = 55;
+user = false;
+user = { id: 1, age: 21, address: "Dhaka" }
+user = [1, 2, 3, 4, 5];
+user = ["rob", "john", "smith"];
+console.log(user);
