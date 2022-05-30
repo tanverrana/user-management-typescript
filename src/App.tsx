c03b849f25d013d1d76a182e98bb13b149c2f816
tanverrana <tanversobur@gmail.com>
2022-05-30 11:53:27 +0600

    using interface

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,23 @@ let isSmart: boolean = true;
 
 let students: String[] = ["tnaver", "Rana"];
 
+const person: { name: string, job: string, age: number } = {
+  name: "Tanver",
+  job: "jobless",
+  age: 85
+}
+
+interface person1 {
+  name: string,
+  job: string,
+  age: number
+}
+const person: person1 = {
+  name: "tanver",
+  job: "jobless",
+  age: 21
+}
+
 function App() {
   return (
     <div className="App">
